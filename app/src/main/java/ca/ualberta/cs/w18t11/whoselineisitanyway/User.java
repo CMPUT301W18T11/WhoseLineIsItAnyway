dab1c3833a0f104a3098ab5d5a80a9a6f22cf2dd
Samuel Dolha <dolha@ualberta.ca>
2018-02-24 23:37:02 -0700

    Implement User.getEmailAddress

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/User.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/User.java
@@ -2,4 +2,15 @@ package ca.ualberta.cs.w18t11.whoselineisitanyway;
 
 final class User
 {
+    private final EmailAddress emailAddress;
+
+    User(final EmailAddress emailAddress, final PhoneNumber phoneNumber)
+    {
+        this.emailAddress = emailAddress;
+    }
+
+    final EmailAddress getEmailAddress()
+    {
+        return this.emailAddress;
+    }
 }
