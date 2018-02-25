09ed27e374ede1dc6f89c5f13e733a9f4c074d64
Samuel Dolha <dolha@ualberta.ca>
2018-02-24 22:55:49 -0700

    Add PhoneNumber getCountryCode test

--- app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/PhoneNumberUnitTest.java
+++ app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/PhoneNumberUnitTest.java
@@ -1,5 +1,15 @@
 package ca.ualberta.cs.w18t11.whoselineisitanyway;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public final class PhoneNumberUnitTest
 {
+    @Test
+    public final void testGetCountryCode()
+    {
+        final int countryCode = 1;
+        Assert.assertEquals(countryCode,
+                new PhoneNumber(countryCode, 555, 123, 6789).getCountryCode());
+    }
 }
