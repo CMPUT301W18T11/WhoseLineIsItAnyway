02c29beb4817db4555e841c7741d7158b26c9178
Samuel Dolha <dolha@ualberta.ca>
2018-02-24 23:36:52 -0700

    Add User getEmailAddress test

--- app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/UserUnitTest.java
+++ app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/UserUnitTest.java
@@ -1,5 +1,15 @@
 package ca.ualberta.cs.w18t11.whoselineisitanyway;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public final class UserUnitTest
 {
+    @Test
+    public final void testGetEmailAddress()
+    {
+        final EmailAddress emailAddress = new EmailAddress("user", "gmail.com");
+        Assert.assertEquals(emailAddress,
+                new User(emailAddress, new PhoneNumber(1, 800, 777, 6543)).getEmailAddress());
+    }
 }
