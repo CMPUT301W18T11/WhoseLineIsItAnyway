1eb317c948df43d60737f5c41643ec480d0a0fad
Samuel Dolha <dolha@ualberta.ca>
2018-02-24 22:38:35 -0700

    Implement EmailAddress.getLocalPart

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/EmailAddress.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/EmailAddress.java
@@ -2,4 +2,15 @@ package ca.ualberta.cs.w18t11.whoselineisitanyway;
 
 final class EmailAddress
 {
+    private final String localPart;
+
+    EmailAddress(final String localPart, final String domain)
+    {
+        this.localPart = localPart;
+    }
+
+    final String getLocalPart()
+    {
+        return this.localPart;
+    }
 }
