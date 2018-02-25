624e62486bb3d4ca5b6265a8540259b852a56280
Samuel Dolha <dolha@ualberta.ca>
2018-02-24 22:58:05 -0700

    Implement PhoneNumber.getAreaCode

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/PhoneNumber.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/PhoneNumber.java
@@ -4,16 +4,24 @@ final class PhoneNumber
 {
     private final int countryCode;
 
+    private final int areaCode;
+
     PhoneNumber(final int countryCode,
                 final int areaCode,
                 final int exchangeCode,
                 final int lineNumber)
     {
         this.countryCode = countryCode;
+        this.areaCode = areaCode;
     }
 
     final int getCountryCode()
     {
         return this.countryCode;
+    }
+
+    final int getAreaCode()
+    {
+        return this.areaCode;
     }
 }
