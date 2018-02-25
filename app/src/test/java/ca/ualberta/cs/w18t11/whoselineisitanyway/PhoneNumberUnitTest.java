6fd64e28deaffa188a508dde58eb7a20ab2080e8
Samuel Dolha <dolha@ualberta.ca>
2018-02-24 23:32:44 -0700

    Add PhoneNumber equals test

--- app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/PhoneNumberUnitTest.java
+++ app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/PhoneNumberUnitTest.java
@@ -41,4 +41,30 @@ public final class PhoneNumberUnitTest
     {
         Assert.assertEquals("+1 (555) 123-6789", new PhoneNumber(1, 555, 123, 6789).toString());
     }
+
+    @Test
+    public final void testEquals()
+    {
+        final int countryCode = 1;
+        final int areaCode = 555;
+        final int exchangeCode = 123;
+        final int lineNumber = 6789;
+        final PhoneNumber firstNumber
+                = new PhoneNumber(countryCode, areaCode, exchangeCode, lineNumber);
+        final PhoneNumber secondNumber
+                = new PhoneNumber(countryCode, areaCode, exchangeCode, lineNumber);
+        final PhoneNumber thirdNumber
+                = new PhoneNumber(2, areaCode, exchangeCode, lineNumber);
+        final PhoneNumber fourthNumber
+                = new PhoneNumber(countryCode, 888, exchangeCode, lineNumber);
+        final PhoneNumber fifthNumber
+                = new PhoneNumber(countryCode, areaCode, 633, 4444);
+        final PhoneNumber sixthNumber
+                = new PhoneNumber(countryCode, areaCode, exchangeCode, 4444);
+
+        Assert.assertEquals(firstNumber, secondNumber);
+        Assert.assertNotEquals(secondNumber, thirdNumber);
+        Assert.assertNotEquals(thirdNumber, fourthNumber);
+        Assert.assertNotEquals(fourthNumber, fifthNumber);
+    }
 }
