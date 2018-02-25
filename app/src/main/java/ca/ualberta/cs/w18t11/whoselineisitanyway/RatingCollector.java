6d770ea34f76a05830ea7147708cf9fdb671204f
Lucas Thalen <lucas.thalen@gmail.com>
2018-02-25 13:22:59 -0700

    ﻿Update <RatingCollector>, <Rating> + Unit Tests Overwrite earlier versions which were incomplete. These files are commented and the unit tests have been coded for both classes.

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/RatingCollector.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/RatingCollector.java
@@ -1,5 +1,4 @@
 package ca.ualberta.cs.w18t11.whoselineisitanyway;
-
 /**
  * RatingCollector is designed to hold and manage overall ratings for any particular user
  * @author Lucas Thalen
@@ -17,17 +16,45 @@ public class RatingCollector {
 
     private ArrayList<Rating> ratings = new ArrayList<Rating>();
 
+    /**
+     * Update Averages across all reviews
+     */
     private void updateAvg() {
         double reviewSum = 0.0;
+        double avgQuality = 0.0;
+        double avgTTC = 0.0;
+        double avgProf = 0.0;
+
         for (int i = 0; i < ratings.size(); i ++) {
             reviewSum += ratings.get(i).getAggRating();
+            avgQuality += ratings.get(i).getQualityRating();
+            avgTTC += ratings.get(i).getTTCRating();
+            avgProf += ratings.get(i).getProfRating();
         }
         reviewSum = reviewSum / ratings.size();
         avgRating = (int) reviewSum;
+        this.avgQuality = (int) (avgQuality / ratings.size());
+        this.avgTTC = (int) (avgTTC / ratings.size());
+        this.avgProf = (int) (avgProf / ratings.size());
+
     }
+
+    /**
+     * Adds a rating to the overall collection for management
+     * @param rate A rating object to be added to the collection
+     */
     public void addRating(Rating rate) {
         ratings.add(rate);
         update();
+    }
+
+    /**
+     * Get a rating from the collection
+     * @param index index of the rating; this should correspond with the listview likely to display
+     * @return Rating at specific index
+     */
+    public Rating getRating(int index) {
+        return ratings.get(index);
     }
     private void update() {
         getAvgProf();
@@ -35,34 +62,50 @@ public class RatingCollector {
         getAvgTTC();
         updateAvg();
     }
-    public void getAvgTTC() {
+
+    /**
+     * Get the overall average Time-to-completion value
+     * @return TTC_average
+     */
+    public int getAvgTTC() {
         double reviewSum = 0.0;
         for (int i = 0; i < ratings.size(); i ++) {
             reviewSum += ratings.get(i).getTTCRating();
         }
         reviewSum = reviewSum / ratings.size();
         avgTTC = (int) reviewSum;
+        return avgTTC;
     }
 
-    public void getAvgProf() {
+    /**
+     * Get the average professionalism rating over all entries
+     * @return The average professionalism rating across all entries
+     */
+    public int getAvgProf() {
         double reviewSum = 0.0;
         for (int i = 0; i < ratings.size(); i ++) {
             reviewSum += ratings.get(i).getProfRating();
         }
         reviewSum = reviewSum / ratings.size();
         avgProf = (int) reviewSum;
+        return avgProf;
     }
 
-    public void getAvgQuality() {
+    /**
+     * Get the average quality across all entries
+     * @return the average quality rating of all entries
+     */
+    public int getAvgQuality() {
         double reviewSum = 0.0;
         for (int i = 0; i < ratings.size(); i ++) {
             reviewSum += ratings.get(i).getQualityRating();
         }
         reviewSum = reviewSum / ratings.size();
         avgQuality = (int) reviewSum;
+        return avgQuality;
     }
 
-    public int reviewCount() {
+    public int getRatingCount() {
         return ratings.size();
     }
 
