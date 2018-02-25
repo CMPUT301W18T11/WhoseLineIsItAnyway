75c9a9f40b3ff58b92fccb0c5ec927239880ed4d
Samuel Dolha <dolha@ualberta.ca>
2018-02-24 22:37:35 -0700

    Add EmailAddress getLocalPart test

--- app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/EmailAddressUnitTest.java
+++ app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/EmailAddressUnitTest.java
@@ -1,7 +1,14 @@
 package ca.ualberta.cs.w18t11.whoselineisitanyway;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public final class EmailAddressUnitTest
 {
+    @Test
+    public final void testGetLocalPart()
+    {
+        final String localPart = "franklin";
+        Assert.assertEquals(localPart, new EmailAddress(localPart, "domain.com").getLocalPart());
+    }
 }
