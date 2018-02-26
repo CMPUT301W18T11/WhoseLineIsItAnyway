fe0488118b1f9ee418f728b542cd4f0f6a03905b
Lucas Thalen <lucas.thalen@gmail.com>
2018-02-26 14:47:30 -0700

    Make unit-tests deterministic <Rating[Collector]> The tests have been edited to remove random values and substitute them for deterministic values; where possible messages have been added using truth statements, allowing a wider breadth of test without losing sight of what triggered a possible failure.

--- app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/RatingUnitTest.java
+++ app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/RatingUnitTest.java
@@ -1,6 +1,6 @@
 package ca.ualberta.cs.w18t11.whoselineisitanyway;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import java.util.Random;
 
@@ -14,12 +14,13 @@ public final class RatingUnitTest
 {
     /**
      * Test to see if negative values (Expected OOB) throws exception when creating new Rating
+     * The code for checking bounds is identical for all of them, so if one throws except, fix all.
      */
     @Test(expected = IllegalArgumentException.class)
     public final void testNewRatingNegativeParam()
     {
         final int failInt = -1 * new Random().nextInt(Integer.MIN_VALUE);
-        Assert.assertTrue(failInt < 0);
+        assertTrue(failInt < 0);
         new Rating(failInt, failInt, failInt);
     }
 
@@ -30,14 +31,9 @@ public final class RatingUnitTest
     public final void testNewRatingPositiveOOB()
     {
         final Random randomPos = new Random();
-        int failInt = randomPos.nextInt(Integer.MAX_VALUE);
+        int failInt = 22;
 
-        while (failInt < 5)
-        {
-            failInt = randomPos.nextInt(Integer.MAX_VALUE);
-        }
-
-        Assert.assertTrue(failInt > 5);
+        assertTrue(failInt > 5);
         new Rating(failInt, failInt, failInt);
     }
 
@@ -47,118 +43,66 @@ public final class RatingUnitTest
     @Test
     public final void testNewRatingZeroFive()
     {
-        final Random randomPos = new Random();
-
-        for (int index = 0; index < 100; index += 1)
-        {
-            final int p1 = randomPos.nextInt(5);
-            final int p2 = randomPos.nextInt(5);
-            final int p3 = randomPos.nextInt(5);
-            Assert.assertTrue(0 <= p1 && p1 <= 5);
-            Assert.assertTrue(0 <= p2 && p2 <= 5);
-            Assert.assertTrue(0 <= p3 && p3 <= 5);
-            new Rating(p1, p2, p3);
-        }
+        int p1 = 0;
+        int p2 = 3;
+        int p3 = 5;
+        assertTrue(0 <= p1 && p1 <= 5);
+        assertTrue(0 <= p2 && p2 <= 5);
+        assertTrue(0 <= p3 && p3 <= 5);
+        new Rating(p1, p2, p3);
     }
 
     // TTC Stuff
     @Test
-    public final void testGetTtc()
-    {
-        final Random randomPos = new Random();
-
-        for (int index = 0; index < 100; index += 1)
-        {
-            final int ttc = randomPos.nextInt(5);
-            final Rating rating = new Rating(0, ttc,0);
-            Assert.assertEquals(rating.getTtcRating(), ttc);
-        }
-    }
-
-    @Test
-    public final void testSetTtc()
-    {
-        final Random posInt = new Random();
+    public final void testSetTtc() {
         final Rating rating = new Rating(0, 0, 0);
-        Assert.assertEquals(rating.getTtcRating(), 0);
-
-        for (int index = 0; index < 100; index += 1)
-        {
-            final int ttc = posInt.nextInt(5);
+        assertEquals(rating.getTtcRating(), 0);
+        for (int ttc = 0; ttc < 6; ttc++) {
             rating.setTtcRating(ttc);
-            Assert.assertEquals(rating.getTtcRating(), ttc);
+            assertTrue("Try setTTC " + ttc, rating.getTtcRating() == ttc);
+
         }
     }
 
     // QUALITY STUFF
     @Test
-    public final void testGetQuality()
-    {
-        final Random randomPos = new Random();
-
-        for (int index = 0; index < 100; index += 1)
-        {
-            final int quality = randomPos.nextInt(5);
-            final Rating rating = new Rating(quality, 0,0);
-            Assert.assertEquals(rating.getQualityRating(), quality);
-        }
-    }
-
-    @Test
     public final void testSetQuality()
     {
-        final Random posInt = new Random();
         final Rating rating = new Rating(0, 0, 0);
-        Assert.assertEquals(rating.getQualityRating(), 0);
+        assertEquals(rating.getQualityRating(), 0);
 
-        for (int index = 0; index < 100; index += 1)
-        {
-            final int quality = posInt.nextInt(5);
-            rating.setQualityRating(quality);
-            Assert.assertEquals(rating.getQualityRating(), quality);
+        for (int quality = 0; quality < 6; quality++) {
+            rating.setTtcRating(quality);
+            assertTrue("Try setQuality " + quality, rating.getTtcRating() == quality);
+
         }
     }
 
     // PROF STUFF
     @Test
-    public final void testGetProf()
-    {
-        final Random randomPos = new Random();
-        for (int index = 0; index < 100; index += 1)
-        {
-            final int prof = randomPos.nextInt(5);
-            final Rating rating = new Rating(0, 0,prof);
-            Assert.assertEquals(rating.getProfRating(), prof);
-        }
-    }
-
-    @Test
     public final void testSetProf()
     {
-        final Random posInt = new Random();
         final Rating rating = new Rating(0, 0, 0);
-        Assert.assertEquals(rating.getProfRating(), 0);
-
-        for (int index = 0; index < 100; index += 1)
-        {
-            final int prof = posInt.nextInt(5);
+        assertEquals(rating.getProfRating(), 0);
+        
+        for (int prof = 0; prof < 6; prof++) {
             rating.setProfRating(prof);
-            Assert.assertEquals(rating.getProfRating(), prof);
+            assertTrue("Try setProf " + prof, rating.getProfRating() == prof);
+
         }
     }
 
     @Test
     public final void testGetAggregateRating()
     {
-        final Random posInt = new Random();
-
-        for (int index = 0; index < 100; index += 1)
-        {
-            final int p1 = posInt.nextInt(5);
-            final int p2 = posInt.nextInt(5);
-            final int p3 = posInt.nextInt(5);
-            final Rating rating = new Rating(p1, p2, p3);
-            Assert.assertEquals(rating.getAggRating(), (int)((p1 + p2 + p3) / 3));
+        for (int p1 = 0; p1 < 6; p1 ++) {
+            for (int p2 = 0; p2 < 6; p2 ++) {
+                for (int p3 = 0; p3 < 6; p3 ++) {
+                    Rating rate = new Rating(p1, p2, p3);
+                    int aggTrueValue = (int) ((p1 + p2 + p3) / 3);
+                    assertTrue("test GetAggregateRating Q: " + p1 + " T:" + p2 + " P:" + p3, rate.getAggRating() == aggTrueValue);
+                }
+            }
         }
     }
 
