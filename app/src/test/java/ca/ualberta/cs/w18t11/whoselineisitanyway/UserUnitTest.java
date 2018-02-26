8a3846825892de2db2025c9843196e825662f5a6
Samuel Dolha <dolha@ualberta.ca>
2018-02-26 15:20:25 -0700

    Implement User.getRequestedTasks and .getAssignedTasks

--- app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/UserUnitTest.java
+++ app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/UserUnitTest.java
@@ -20,4 +20,16 @@ public final class UserUnitTest
         Assert.assertEquals(phoneNumber,
                 new User(new EmailAddress("user", "gmail.com"), phoneNumber).getPhoneNumber());
     }
+
+    @Test
+    public final void testGetTasks()
+    {
+        final User user = new User(
+                new EmailAddress("user", "gmail.com"), new PhoneNumber(3, 333, 333, 3333));
+        final Manager requestedTasks = user.getRequestedTasks();
+        final Manager assignedTasks = user.getAssignedTasks();
+        Assert.assertNotNull(requestedTasks);
+        Assert.assertNotNull(assignedTasks);
+        Assert.assertNotEquals(requestedTasks, assignedTasks);
+    }
 }
