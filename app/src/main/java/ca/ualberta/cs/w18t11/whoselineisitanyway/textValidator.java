51809e4fcb6d0caaa1f67ef63e376125e72c6ad7
Lucas Thalen <lucas.thalen@gmail.com>
2018-03-12 20:13:06 -0600

    Add eMail and PhoneNum Validations As these are important for the CreateUserUI, adding this information to the class. Designed as a class to provide a single point of failure for the regex and ensure consistency in the format of text objects.

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/textValidator.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/textValidator.java
@@ -7,31 +7,36 @@ package ca.ualberta.cs.w18t11.whoselineisitanyway;
 import java.util.regex.*;
 
 public class TextValidator {
-    /**
-     * Hard/soft match of Date (as string)
-     * This variant of Date matching will either allow partial matching or enforce full matching based on LenientMatch
-     * it will return false.
-     *
-     * @param input           string containing a date to be evaluated
-     * @param partialMatching Boolean indicating whether full matching is enforced
-     * @return True (matches a date) or False (fails regex or is too short)
-     */
-    public boolean matchCalendarDate(final String input, final boolean partialMatching) {
-        String regPatt = "^(0[1-9]|1[0-2])\\/(0[1-9]|1\\d|2\\d|3[0-1])\\/(\\d{4})"; // 0 = month, 1 = day, 2 = year
-        Pattern datePattern = Pattern.compile(regPatt);
 
-        Matcher matcher = datePattern.matcher(input);
-        boolean res = matcher.matches();
-        if (res) {
-            int month = Integer.getInteger(matcher.group(0));
-            int day = Integer.getInteger(matcher.group(1));
-            int year = Integer.getInteger(matcher.group(2));
-            if ((month == 4 || month == 6 || month == 9 || month == 1) && day > 30) { return false; }
-            if (month == 2 && day == 29 && !(year % 4 == 0)) { return false; }
+    public int validatePhoneNumber(final String input, final boolean allowPartialMatching) {
+
+        Pattern phoneNum = Pattern.compile("^\\+(\\d)+ \\((\\d{3})\\) (\\d{3})\\-(\\d{4})");
+        Matcher matches = phoneNum.matcher(input);
+        boolean res = matches.matches();
+
+        if (res) { return 1; }
+        else {
+            if (matches.hitEnd() && allowPartialMatching) {
+                return 1;
+            } else {
+                return -1;
+            }
         }
+    }
 
+    // Note: real email is horribly complicated, but this will validate that basic components exist.
+    public int validateEmail(final String input, final boolean allowPartialMatching) {
+        Pattern eMail = Pattern.compile("^([^@]+)@([^@]+\\.[^@]+)");
+        Matcher matches = eMail.matcher(input);
+        boolean res = matches.matches();
 
-
-        return matcher.hitEnd();
+        if (res) { return 1; }
+        else {
+            if (matches.hitEnd() && allowPartialMatching) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
     }
 }
--- app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/TextValidatorTest.java
+++ app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/TextValidatorTest.java
@@ -11,93 +11,131 @@ public final class TextValidatorTest {
     final TextValidator validator = new TextValidator();
 
     @Test
-    public void partialDate_LeapYear() {
+    public void phoneNum_PartialIncompleteCorrectFormat() {
+        String phone1 = "+1 (780) 4";
+        String phone2 = "";
+        String phone3 = "+1 ";
+        
+        assertEquals(phone1, 1, validator.validatePhoneNumber(phone1, true));
+        assertEquals(phone2, 1, validator.validatePhoneNumber(phone2, true));
+        assertEquals(phone3, 1, validator.validatePhoneNumber(phone3, true));
+        
+    }
+    @Test
+    public void phoneNum_PartialWrongEnds() {
+        String phone1 = ".1 (780) 111-1111";
+        String phone2 = "+1 (909) 111-111.";
 
+        assertEquals(phone1, -1, validator.validatePhoneNumber(phone1, true));
+        assertEquals(phone2, -1, validator.validatePhoneNumber(phone2, true));
+    }
+    @Test
+    public void phoneNum_PartialIncompleteWrongFormat() {
+        String phone1 = "+1 (780. 111-111";
+        String phone2 = "780 111-111";
+        String phone3 = "+1 (780)111-111";
 
-        String leapyear = "02/29";
-        String leapyear2 = "02/29/2";
+        assertEquals(phone1, -1, validator.validatePhoneNumber(phone1, true));
+        assertEquals(phone2, -1, validator.validatePhoneNumber(phone2, true));
+        assertEquals(phone3, -1, validator.validatePhoneNumber(phone3, true));
+    }
+    @Test
+    public void phoneNum_PartialCorrectFormat() {
+        String phone1 = "+22 (627) 111-2222";
+        String phone2 = "+1 (780) 432-3399";
 
-        assertEquals(leapyear, true, validator.matchCalendarDate(leapyear, true));
-        assertEquals(leapyear2, true, validator.matchCalendarDate(leapyear2, true));
+        assertEquals(phone1, 1, validator.validatePhoneNumber(phone1, true));
+        assertEquals(phone2, 1, validator.validatePhoneNumber(phone2, true));
+    }
+    @Test
+    public void phoneNum_NoPartialIncompleteCorrectFormat() {
+        String phone1 = "+1 (780) 4";
+        String phone2 = "";
+        String phone3 = "+1 ";
 
+        assertEquals(phone1, -1, validator.validatePhoneNumber(phone1, false));
+        assertEquals(phone2, -1, validator.validatePhoneNumber(phone2, false));
+        assertEquals(phone3, -1, validator.validatePhoneNumber(phone1, false));
+    }
+    @Test
+    public void phoneNum_NoPartialIncompleteWrongFormat() {
+        String phone1 = "+1 .780";
+        String phone2 = "+l";
+        String phone3 = "+1 )780) 111-1.11";
+
+        assertEquals(phone1, -1, validator.validatePhoneNumber(phone1, false));
+        assertEquals(phone2, -1, validator.validatePhoneNumber(phone2, false));
+        assertEquals(phone3, -1, validator.validatePhoneNumber(phone1, false));
+        
+    }
+    @Test
+    public void phoneNum_NoPartialCompleteCorrectFormat() {
+        String phone1 = "+1 (780) 432-9999";
+        String phone2 = "+22 (909) 861-2245";
+
+        assertEquals(phone1, 1, validator.validatePhoneNumber(phone1, false));
+        assertEquals(phone2, 1, validator.validatePhoneNumber(phone2, false));
     }
 
     @Test
-    public void partialDate_WrongFormatIncomplete() {
-        ArrayList<String> tests = new ArrayList<String>();
-        tests.add("02/25/.");
-        tests.add("0.");
-        tests.add("01/.");
-        tests.add("0/1/");
-        tests.add("01/01/2.0");
+    public void email_PartialIncompleteWrongFormat() {
+        String email1 = "mt@hurn@live.c";
+        String email2 = "att.ne@@t";
 
-        for (int i = 0; i < tests.size(); i ++) {
-            assertEquals(tests.get(i), false, validator.matchCalendarDate(tests.get(i), true));
+        assertEquals(email1, -1 , validator.validateEmail(email1, true));
+        assertEquals(email2, -1 , validator.validateEmail(email2, true));
+    }
+    @Test
+    public void email_PartialCompleteCorrectFormat() {
+        ArrayList<String> emailList = new ArrayList<String>();
+        // Note: Randomly Generated, with a few stoppers thrown in
+        emailList.add("bjoern@me.com");
+        emailList.add("attwood@aol.com");
+        emailList.add("mrdvt@msn.com");
+        emailList.add("sfoskett@yahoo.ca");
+        emailList.add("netsfr@live.com");
+        emailList.add("tokuhirom@optonline.net");
+        emailList.add("osaru@verizon.net");
+        emailList.add("neonatus@optonline.net");
+        emailList.add("bri-ckbat@hotmail.com");
+        emailList.add("yangyan@hotmail.com");
+        emailList.add("rhavyn@yahoo.com");
+        emailList.add("mwitte@att.net");
+        emailList.add("pkilab@mac.com");
+        emailList.add("wbarker@sbcglobal.net");
+        emailList.add("gr+dschl@yahoo.com");
+        emailList.add("damian@1234g.com");
+        emailList.add("do.ne.v@yahoo.ca");
+        emailList.add("zeller@hotmail.com");
+        emailList.add("isaacson@msn.com");
+        emailList.add("cosimo@comcast.net");
+        emailList.add("miltchev@msn.com");
+        emailList.add("jonas@me.com");
+        emailList.add("sriha@gmail.com");
+        emailList.add("hakim@hotmail.com");
+        emailList.add("fudrucker@aol.com");
+        emailList.add("naupa@hotmail.com");
+        emailList.add("djupedal@mac.126");
+        emailList.add("kawasaki@msn.com");
+        emailList.add("tellis@aol.com");
+        emailList.add("bjoern@msn.com");
+
+        for (int i = 0; i < emailList.size(); i ++) {
+            assertEquals(emailList.get(i), 1, validator.validateEmail(emailList.get(i), true));
         }
     }
-
     @Test
-    public void partialDate_WrongFormatComplete() {
-        ArrayList<String> tests = new ArrayList<String>();
-        tests.add("01/.2/2018");
-        tests.add("0p/01/1970");
-        tests.add("0123456789/");
+    public void email_PartialIncompleteCorrectFormat() {
+        ArrayList<String> emailList = new ArrayList<String>();
+        // Note: Randomly Generated, with a few stoppers thrown in
+        emailList.add("bjoern@");
+        emailList.add("attwood@aol.");
+        emailList.add("m.r.dv.t@msn.com");
+        emailList.add("sfos123kett@yaho12233oa");
 
-        for (int i = 0; i < tests.size(); i ++) {
-            assertEquals(tests.get(i), false, validator.matchCalendarDate(tests.get(i), true));
-        }
-    }
 
-    @Test
-    public void partialDate_CorrectFormatIncomplete() {
-        ArrayList<String> tests = new ArrayList<String>();
-        tests.add("02/25/");
-        tests.add("01");
-        tests.add("01/");
-        tests.add("01/31/20");
-
-        for (int i = 0; i < tests.size(); i ++) {
-            assertEquals(tests.get(i), true, validator.matchCalendarDate(tests.get(i), true));
-        }
-    }
-
-    @Test
-    public void partialDate_WrongFormatFront() {
-        ArrayList<String> tests = new ArrayList<String>();
-        tests.add("/2/25/");
-        tests.add(".1");
-        tests.add("/01/");
-        tests.add("0-/31/20");
-
-        for (int i = 0; i < tests.size(); i ++) {
-            assertEquals(tests.get(i), false, validator.matchCalendarDate(tests.get(i), true));
-        }
-    }
-    @Test
-    public void partialDate_WrongFormatEnd() {
-        ArrayList<String> tests = new ArrayList<String>();
-        tests.add("02/25/....");
-        tests.add("01/03/201.");
-        tests.add("01/.......");
-
-        for (int i = 0; i < tests.size(); i ++) {
-            assertEquals(tests.get(i), false, validator.matchCalendarDate(tests.get(i), true));
-        }
-    }
-
-    @Test
-    public void partialDate_BoundsWrong() {
-        ArrayList<String> tests = new ArrayList<String>();
-        tests.add("02/32/20");
-        tests.add("00/10/20");
-        tests.add("13/10/20");
-        tests.add("04/31/");
-        tests.add("06/31/2");
-        tests.add("11/31/2");
-        tests.add("09/31/201");
-
-        for (int i = 0; i < tests.size(); i ++) {
-            assertEquals(tests.get(i), false, validator.matchCalendarDate(tests.get(i), true));
+        for (int i = 0; i < emailList.size(); i ++) {
+            assertEquals(emailList.get(i), 1, validator.validateEmail(emailList.get(i), true));
         }
     }
 }
