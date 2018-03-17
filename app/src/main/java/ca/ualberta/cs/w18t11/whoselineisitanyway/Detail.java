dbb81bf8fe61eab2c444847f1ebd1278e145f7dc
Brad Ofrim <bofrim@gmail.com>
2018-03-17 13:34:19 -0600

    Implemets Detail and adds tests

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/Detail.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/Detail.java
@@ -1,5 +1,27 @@
 package ca.ualberta.cs.w18t11.whoselineisitanyway;
 
 
+import android.content.Intent;
+
 public class Detail {
+    private final String title;
+    private final String info;
+    private final Intent linkIntent;
+    Detail(String title, String info, Intent linkIntent)
+    {
+        this.title = title;
+        this.info = info;
+        this.linkIntent = linkIntent;
+    }
+
+    Detail(String title, String info)
+    {
+        this(title, info, null);
+    }
+
+    String getTitle() {return this.title;}
+
+    String getInfo() {return this.info;}
+
+    boolean isLinkeable(){return this.linkIntent != null;}
 }
--- app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/DetailTest.java
+++ app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/DetailTest.java
@@ -1,4 +1,35 @@
 package ca.ualberta.cs.w18t11.whoselineisitanyway;
 
+import android.content.Intent;
+
+import org.junit.Test;
+import org.junit.Assert;
+
 public class DetailTest {
+    @Test
+    public final void testGetTitle()
+    {
+        final String title = "Detail";
+        Assert.assertEquals(title, new Detail(title, "").getTitle());
+    }
+
+    @Test
+    public final void testGetInfo()
+    {
+        final String info = "Info";
+        Assert.assertEquals(info, new Detail("", info).getInfo());
+    }
+
+    @Test
+    public final void testIsLinkable_notLinkable()
+    {
+        Assert.assertFalse(new Detail("", "").isLinkeable());
+    }
+
+    @Test
+    public final void testIsLinkable_Linkable()
+    {
+        Intent i = new Intent();
+        Assert.assertTrue(new Detail("", "", i).isLinkeable());
+    }
 }
