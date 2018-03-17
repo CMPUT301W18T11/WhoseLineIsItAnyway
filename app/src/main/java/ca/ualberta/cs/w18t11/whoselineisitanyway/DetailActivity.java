03da82a2bcf732907cf77c4007079971015691fe
Brad Ofrim <bofrim@gmail.com>
2018-03-17 16:40:01 -0600

    Supports a detail list

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/DetailActivity.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/DetailActivity.java
@@ -1,13 +1,66 @@
 package ca.ualberta.cs.w18t11.whoselineisitanyway;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class DetailActivity extends AppCompatActivity {
 
+    TextView title;
+    ListView detailList;
+    ArrayList<Detail> details;
+
+    /**
+     * Preform any activities needed on creation
+     * @param savedInstanceState saved instance state
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        initializeUserInterface();
+        setupFromIntent();
+        customizeUserInterface();
+    }
+
+    /**
+     * Customize the user interface by adding buttons or modifying default elements
+     * To be overridden if needed, otherwise, leave it blank
+     */
+    public void customizeUserInterface() {
+        // Override
+    }
+
+    /**
+     * Initialize the user interface by setting the values for the default elements
+     */
+    private void initializeUserInterface()
+    {
+        title = findViewById(R.id.textview_activity_detail_title);
+        title.setText("Detail");
+        detailList = findViewById(R.id.listview_activity_detail_list);
+        detailList.setAdapter(new DetailRowAdapter(this, details));
+    }
+
+    /**
+     * Grab data from the incoming intent
+     */
+    private void setupFromIntent()
+    {
+        Intent intent = getIntent();
+        details = (ArrayList<Detail>) intent.getSerializableExtra(Detailable.DATA_DETAIL_LIST);
+        title.setText(intent.getStringExtra(Detailable.DATA_DETAIL_TITLE));
     }
 }
