177ad67b765e86dede1dd8870b69ed1f9e7616bf
Brad Ofrim <bofrim@gmail.com>
2018-03-17 14:05:07 -0600

    Adds strings for intent passing

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/Detailable.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/Detailable.java
@@ -6,6 +6,10 @@ import android.util.Pair;
 import java.util.ArrayList;
 
 public interface Detailable {
+    String DATA_DETAIL_TITLE = "com.whoselineisitanyway.DATA_DETAIL_TITLE";
+    String DATA_DETAIL_LIST = "com.whoselineisitanyway.DATA_DETAIL_LIST";
+    String DATA_DETAIL_IMAGE = "com.whoselineisitanyway.DATA_DETAIL_IMAGE";
+
     /**
      * Get the a list containing the details about the object
      * @return ArrayList<Pair<String, String>> representation of the object's details
