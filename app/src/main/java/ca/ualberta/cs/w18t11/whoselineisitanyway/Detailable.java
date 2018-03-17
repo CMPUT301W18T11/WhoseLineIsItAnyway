627eb77df41c1d34f38aca450c6a1fa9ed79eb2b
Brad Ofrim <bofrim@gmail.com>
2018-03-17 12:11:08 -0600

    Modifys Detailable interface

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/Detailable.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/Detailable.java
@@ -1,9 +1,23 @@
 package ca.ualberta.cs.w18t11.whoselineisitanyway;
 
-import java.util.HashMap;
+import android.content.Intent;
+import android.util.Pair;
+
+import java.util.ArrayList;
 
 public interface Detailable {
-    HashMap<String, String> getDetails();
+    /**
+     * Get the a list containing the details about the object
+     * @return ArrayList<Pair<String, String>> representation of the object's details
+     * where the Pair contains (Detail title, Detail data)
+     */
+    ArrayList<Detail> getDetails();
 
-    boolean hasDetailImage();
+    /**
+     * View the details of the Detailable
+     * Go to a new DetailActivity representing the object
+     * @param detailActivity the type of activity to display the object's details
+     * @return boolean representing if the object is represented by an image
+     */
+    boolean showDetail(DetailActivity detailActivity);
 }
