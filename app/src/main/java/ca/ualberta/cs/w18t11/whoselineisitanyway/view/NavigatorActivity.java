56f07b46d94cbe21ddb75cc67de4de7424d5a2d7
Brad Ofrim <bofrim@gmail.com>
2018-03-19 10:49:54 -0600

    Utilizes MockDataSource in Navigator

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/view/NavigatorActivity.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/view/NavigatorActivity.java
@@ -17,10 +17,13 @@ import android.widget.FrameLayout;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import ca.ualberta.cs.w18t11.whoselineisitanyway.R;
 import ca.ualberta.cs.w18t11.whoselineisitanyway.model.Detailable;
 import ca.ualberta.cs.w18t11.whoselineisitanyway.model.bid.Bid;
+import ca.ualberta.cs.w18t11.whoselineisitanyway.model.datasource.DataSource;
+import ca.ualberta.cs.w18t11.whoselineisitanyway.model.datasource.MockDataSource;
 import ca.ualberta.cs.w18t11.whoselineisitanyway.model.task.Task;
 import ca.ualberta.cs.w18t11.whoselineisitanyway.model.task.TaskStatus;
 import ca.ualberta.cs.w18t11.whoselineisitanyway.model.user.EmailAddress;
@@ -120,53 +123,19 @@ public class NavigatorActivity extends AppCompatActivity
         Intent outgoingIntent = new Intent(this, DetailableListActivity.class);
         String outgoingTitle = "List";
         ArrayList<Detailable> detailables = new ArrayList<>();
-        // TODO: need to load allTasks and allBids with data from data source
-        ArrayList<Task> allTasks = new ArrayList<>();
-        ArrayList<Bid> allBids = new ArrayList<>();
+        DataSource dataSource = new MockDataSource();
+        Task[] allTasks = dataSource.getAllTasks();
+        Bid[] allBids = dataSource.getAllBids();
+
         // TODO: need to account for current user based on whoever is logged in
         User currentUser = new User(new EmailAddress("bob", "gmail.com"),
                 new PhoneNumber(0, 123, 456, 7890), "bob");
-
-
-        // Hardcoded values for demo: ** Remove later **
-        // Get the current user:
-        User providerUserA = new User(new EmailAddress("alice", "gmail.com"),
-                new PhoneNumber(0, 123, 456, 7890), "alice");
-        User providerUserB = new User(new EmailAddress("eve", "gmail.com"),
-                new PhoneNumber(0, 123, 456, 7890), "eve");
-        // Pull all info for tasks and bids
-        // List of the Bids:
-        Bid bid1a = new Bid(providerUserA.getUsername(), "task1ID", new BigDecimal(5));
-        Bid bid1b = new Bid(providerUserB.getUsername(), "task1ID", new BigDecimal(6));
-        Bid bid2b = new Bid(providerUserB.getUsername(), "task2ID", new BigDecimal(500));
-        Bid bid2c = new Bid(currentUser.getUsername(), "task2ID", new BigDecimal(750));
-        Bid bid3b = new Bid(providerUserB.getUsername(), "task3ID", new BigDecimal(5));
-        Bid bid3c = new Bid(currentUser.getUsername(), "task3ID", new BigDecimal(7));
-
-        allBids.add(bid1a);
-        allBids.add(bid1b);
-        allBids.add(bid2b);
-        allBids.add(bid2c);
-        allBids.add(bid3b);
-        allBids.add(bid3c);
-
-        Bid[] mockBidList = {bid3b, bid3c};
-
-        //List of tasks:
-        Task task1 = new Task("id1", currentUser.getUsername(), "Demo Task 1", "A really good task");
-        Task task2 = new Task("id2", providerUserA.getUsername(), "Demo Task 2", "A really great task");
-        Task task3 = new Task("id3", providerUserA.getUsername(), currentUser.getUsername(), mockBidList, "Demo Task 3", "A alright task", Boolean.FALSE);
-
-        allTasks.add(task1);
-        allTasks.add(task2);
-        allTasks.add(task3);
-
 
         if (id == R.id.all_tasks)
         {
             Log.i("NAVBAR: ", "All Tasks Selected");
             outgoingTitle = "All Tasks";
-            detailables.addAll(allTasks);
+            detailables.addAll(Arrays.asList(allTasks));
 
         }
         else if (id == R.id.my_tasks)
