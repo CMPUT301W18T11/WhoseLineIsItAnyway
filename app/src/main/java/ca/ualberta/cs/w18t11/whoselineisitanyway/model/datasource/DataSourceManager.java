833885c0cf0a012908899168e9e2311de5d94456
Brad Ofrim <bofrim@gmail.com>
2018-03-19 14:29:21 -0600

    Adds docs to DataSourceManager

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/model/datasource/DataSourceManager.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/model/datasource/DataSourceManager.java
@@ -4,16 +4,37 @@ import android.util.Log;
 
 import ca.ualberta.cs.w18t11.whoselineisitanyway.model.user.User;
 
+/**
+ * Manages the remote and local data sources used withing the application
+ *
+ * @author Brad Ofrim
+ * @version 1.0
+ */
 public class DataSourceManager {
 
+    /**
+     * Singleton instance
+     */
     private static DataSourceManager instance;
+
+    /**
+     * The server
+     */
     private DataSource remoteDataSource;
+
+    /**
+     * A local copy of data used when remote is not accessible
+     */
     private DataSource localDataSource;
+
+    /**
+     * Indication of who the current user is
+     */
     private User currentUser;
 
     //TODO: Ensure all commands check if we can grab or set data in remote before using local
     /**
-     * Creates a DataSourceManager.
+     * Creates a DataSourceManager
      *
      * @see DataSource
      */
@@ -25,7 +46,7 @@ public class DataSourceManager {
 
     /**
      * @return DataSourceManager the singleton copy of the data source manager
-      */
+     */
     public static DataSourceManager getInstance()
     {
         if(DataSourceManager.instance != null)
@@ -37,7 +58,7 @@ public class DataSourceManager {
     }
 
     /**
-     * Syncronize and changes made to the local data source with the remote
+     * Synchronize and changes made to the local data source with the remote.
      * @return boolean representing if the synchronize was successful
      */
     public boolean synchronizeDataSources(){
