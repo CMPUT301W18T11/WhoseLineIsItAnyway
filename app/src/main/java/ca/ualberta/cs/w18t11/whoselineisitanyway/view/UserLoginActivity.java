a850ae3cea7ed25ba86001f993e2cca9d12e6c61
Mark Griffith <mgriffit@ualberta.ca>
2018-03-22 13:59:32 -0600

    Refactor UserLoginActivity to use ElasticSearchUserController

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/view/UserLoginActivity.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/view/UserLoginActivity.java
@@ -1,24 +1,26 @@
 package ca.ualberta.cs.w18t11.whoselineisitanyway.view;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
 import ca.ualberta.cs.w18t11.whoselineisitanyway.R;
+import ca.ualberta.cs.w18t11.whoselineisitanyway.controllers.ElasticSearchUserController;
 import ca.ualberta.cs.w18t11.whoselineisitanyway.model.datasource.DataSourceManager;
+import ca.ualberta.cs.w18t11.whoselineisitanyway.model.user.EmailAddress;
+import ca.ualberta.cs.w18t11.whoselineisitanyway.model.user.PhoneNumber;
+import ca.ualberta.cs.w18t11.whoselineisitanyway.model.user.User;
 
 
 /**
@@ -32,7 +34,7 @@ public class UserLoginActivity extends AppCompatActivity
     };
 
     // Keep track of the login task to ensure we can cancel it if requested
-    private UserLoginTask authTask = null;
+//    private UserLoginTask authTask = null;
 
     private EditText txtUsername;
     private View progressView;
@@ -95,11 +97,6 @@ public class UserLoginActivity extends AppCompatActivity
      */
     private void attemptLogin()
     {
-        if (authTask != null)
-        {
-            return;
-        }
-
         // Reset errors.
         txtUsername.setError(null);
 
@@ -110,14 +107,11 @@ public class UserLoginActivity extends AppCompatActivity
         View focusView = null;
 
         // Check for a valid email address.
-        if (TextUtils.isEmpty(username))
-        {
+        if (TextUtils.isEmpty(username)) {
             txtUsername.setError(getString(R.string.error_field_required));
             focusView = txtUsername;
             cancel = true;
-        }
-        else if (!isUsernameValid(username))
-        {
+        } else if (!isUsernameValid(username)) {
             txtUsername.setError(getString(R.string.error_invalid_username));
             focusView = txtUsername;
             cancel = true;
@@ -128,14 +122,45 @@ public class UserLoginActivity extends AppCompatActivity
             // There was an error; don't attempt login and focus the first
             // form field with an error.
             focusView.requestFocus();
-        }
-        else
-        {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            showProgress(true);
-            authTask = new UserLoginTask(this, username);
-            authTask.execute((Void) null);
+        } else {
+            // attempt authentication against a network
+            String query = "{" +
+                    "  \"query\": {" +
+                    "    \"match\": {" +
+                    "      \"username\": \"" + username + "\"" +
+                    "    }" +
+                    "  }" +
+                    "}";
+
+            // Get the user with username from the database
+            ElasticSearchUserController.GetMultipleUsersTask getUsersTask
+                    = new ElasticSearchUserController.GetMultipleUsersTask();
+            getUsersTask.execute(query);
+
+            try {
+                ArrayList<User> users = getUsersTask.get();
+                if(users != null){
+                    if (users.size() == 1){
+                        Log.i("UserLogin", "User exists. Attempting login...");
+                        loginUser(users.get(0));
+                    }
+                    else if (users.size() > 1){
+                        Log.i("UserLogin Error", "Username appears more than once in the database");
+                    } else {
+                        Log.i("UserLogin", "Attempting user registration...");
+                        registerUser(username);
+                    }
+                } else {
+                    // TODO offline logic
+                    Log.i("UserLogin", "Initiating offline behaviour...");
+                }
+            }
+            catch (InterruptedException e) {
+                Log.i("UserLogin", "Login interrupted:" + e.toString());
+            }
+            catch (ExecutionException e) {
+                Log.i("UserLogin", "getUsersTask exception:" + e.toString());
+            }
         }
     }
 
@@ -145,123 +170,38 @@ public class UserLoginActivity extends AppCompatActivity
         return !username.contains("A Bad Word");
     }
 
-    /**
-     * Shows the progress UI and hides the login form.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show)
-    {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2)
-        {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            loginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter()
-            {
-                @Override
-                public void onAnimationEnd(Animator animation)
-                {
-                    loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            progressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter()
-            {
-                @Override
-                public void onAnimationEnd(Animator animation)
-                {
-                    progressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        }
-        else
-        {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
+    private void loginUser(User user) {
+        // Set the current user and start the app
+        DataSourceManager.getInstance().setCurrentUser(user.getUsername());
+        Intent intent = new Intent(this, NavigatorActivity.class);
+        this.startActivity(intent);
     }
 
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean>
-    {
+    private void registerUser(String username) {
+        // TODO refactor this to use the createUserActivity
+        User user = new User(new EmailAddress("DefaultLocalPart","Domain@Default.com"),
+                new PhoneNumber(0,0,0,0),
+                username);
 
-        private final String username;
-        private final Context context;
+        // Add the user to database
+        ElasticSearchUserController.AddUsersTask addUserTask = new ElasticSearchUserController.AddUsersTask();
+        addUserTask.execute(user);
 
-        UserLoginTask(Context context, String username)
-        {
-            this.context = context;
-            this.username = username;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params)
-        {
-            // TODO: attempt authentication against a network service
-
-            try
-            {
-                // Simulate network access
-                Thread.sleep(2000);
-            }
-            catch (InterruptedException e)
-            {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS)
-            {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(username))
-                {
-                    // Account exists, return true if the password matches.
-                    DataSourceManager.getInstance().setCurrentUser(username);
-                    Intent intent = new Intent(context, NavigatorActivity.class);
-                    context.startActivity(intent);
-                    return true;
-                }
-            }
-
-            // TODO: register the new account here.
-
-            return false;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success)
-        {
-            authTask = null;
-            showProgress(false);
-
-            if (success)
-            {
-                finish();
-            }
-            else
-            {
-                txtUsername.setError(getString(R.string.error_invalid_username));
-                txtUsername.requestFocus();
+        try {
+            String jId = addUserTask.get();
+            if (jId != null) {
+                // TODO add new user to dataManager
+                loginUser(user);
+            } else {
+                Log.i("UserLogin", "registed user JestId is null");
             }
         }
-
-        @Override
-        protected void onCancelled()
-        {
-            authTask = null;
-            showProgress(false);
+        catch (InterruptedException e) {
+            Log.i("UserLogin", "Login interrupted:" + e.toString());
         }
-
+        catch (ExecutionException e) {
+            Log.i("UserLogin", "addUserTask exception:" + e.toString());
+        }
     }
 }
 
