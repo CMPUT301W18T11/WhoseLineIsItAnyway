09e444920efcf84a157611887aebd8a8e34ebaf8
Mark Griffith <mgriffit@ualberta.ca>
2018-03-23 11:21:51 -0600

    Add skeleton to ElasticSearchBidController

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/controllers/ElasticSearchBidController.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/controllers/ElasticSearchBidController.java
@@ -1,8 +1,84 @@
 package ca.ualberta.cs.w18t11.whoselineisitanyway.controllers;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
+import com.searchly.jestdroid.JestDroidClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.ualberta.cs.w18t11.whoselineisitanyway.model.bid.Bid;
+import ca.ualberta.cs.w18t11.whoselineisitanyway.model.user.User;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+
 /**
- * Created by Marky on 2018-03-23.
+ * Elastic Search controller for handling Task queries
+ *
+ * @author Mark Griffith
+ * @version 1.0
  */
 
 public class ElasticSearchBidController {
+    private static String typeStr = "tasks";
+    private static String idxStr = "cmput301w18t11_whoselineisitanyways";
+    private static JestDroidClient client;
+
+    public static class GetBidByIdTask extends AsyncTask<String, Void, Bid> {
+
+        @Override
+        protected Bid doInBackground(String... bidId) {
+            verifyConfig();
+        }
+
+    }
+
+    public static class GetBidsTask extends AsyncTask<String, Void, ArrayList<Bid>> {
+
+        @Override
+        protected ArrayList<Bid> doInBackground(String... query) {
+            verifyConfig();
+        }
+    }
+
+    public static class AddBidsTask extends AsyncTask<Bid, Void, String> {
+
+        @Override
+        protected String doInBackground(Bid... bids) {
+            verifyConfig();
+        }
+    }
+
+    public static class UpdateBidTask extends AsyncTask<Bid, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Bid... bid) {
+            verifyConfig();
+        }
+
+    }
+
+    public static class RemoveBidTask extends AsyncTask<Bid, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Bid... bid) {
+            verifyConfig();
+        }
+    }
+
+    public static void verifyConfig() {
+        if (client == null) {
+            Log.i("ElasticSearch", "verifying config...");
+            DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080");
+            JestClientFactory factory = new JestClientFactory();
+
+            DroidClientConfig config = builder.build();
+            factory.setDroidClientConfig(config);
+            client = (JestDroidClient) factory.getObject();
+        }
+    }
+
 }
