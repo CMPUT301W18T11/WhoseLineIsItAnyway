66f9ce579818216a9d23dc2010b6ce711f345cb8
Samuel Dolha <dolha@ualberta.ca>
2018-03-24 12:48:39 -0600

    Document Elastic

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/model/elastic/Elastic.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/model/elastic/Elastic.java
@@ -4,15 +4,23 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 /**
- * Represents that which can be stored in elasticsearch.
+ * Represents that which can be stored in Elasticsearch.
  *
  * @author Samuel Dolha
- * @version 1.0
+ * @version 1.1
  */
 public interface Elastic
 {
+    /**
+     * @return The object's elastic ID.
+     */
     @Nullable
     String getElasticId();
 
+    /**
+     * Sets the object's elastic ID.
+     *
+     * @param id The object's new elastic ID.
+     */
     void setElasticId(@NonNull final String id);
 }
