e76e7f79fc8571ab29e1f57c5b24f402908768b2
Samuel Dolha <dolha@ualberta.ca>
2018-03-24 12:44:34 -0600

    Document TaskStatus

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/model/task/TaskStatus.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/model/task/TaskStatus.java
@@ -2,24 +2,57 @@ package ca.ualberta.cs.w18t11.whoselineisitanyway.model.task;
 
 import android.support.annotation.NonNull;
 
+/**
+ * Represents the status of a task.
+ *
+ * @author Samuel Dolha
+ * @version 2.0
+ */
 public enum TaskStatus
 {
+    /**
+     * The initial status of a task, having no bids.
+     */
     REQUESTED("Requested"),
+
+    /**
+     * The status of a task with at least one bid.
+     */
     BIDDED("Bidded"),
+
+    /**
+     * The status of a task with a provider.
+     */
     ASSIGNED("Assigned"),
+
+    /**
+     * The status of a completed task.
+     */
     DONE("Done");
 
-    private final String description;
+    /**
+     * A string representation of the task's status.
+     */
+    private final String representation;
 
-    TaskStatus(@NonNull final String description)
+    /**
+     * Creates a task's status.
+     *
+     * @param representation A string representation of the task's status.
+     */
+    TaskStatus(@NonNull final String representation)
     {
-        this.description = description;
+        this.representation = representation;
     }
 
+    /**
+     * @return A string representation of a task's status.
+     * @see Object
+     */
     @NonNull
     @Override
     public final String toString()
     {
-        return this.description;
+        return this.representation;
     }
 }
