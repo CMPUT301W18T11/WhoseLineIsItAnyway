96e70ceeed45f6cb2034be1334c1db47ac8f8ded
Brad Ofrim <bofrim@gmail.com>
2018-04-05 18:08:40 -0600

    Makes Detail parcelable instead of serializable.

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/model/detail/Detail.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/model/detail/Detail.java
@@ -2,6 +2,8 @@ package ca.ualberta.cs.w18t11.whoselineisitanyway.model.detail;
 
 
 import android.content.Intent;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
@@ -13,7 +15,7 @@ import java.io.Serializable;
  * @author Brad Ofrim, Samuel Dolha
  * @version 2.0
  */
-public final class Detail implements Serializable
+public final class Detail implements Parcelable
 {
     /**
      * An auto-generated, unique ID to support class versioning for Serializable.
@@ -76,5 +78,49 @@ public final class Detail implements Serializable
     public final boolean isLinked()
     {
         return this.linkingIntent != null;
+    }
+
+    /**
+     * Implement for Parcelable
+     * @return
+     */
+    public int describeContents() {
+        return 0;
+    }
+
+    /**
+     * Implement for Parcelable
+     * @return
+     */
+    public void writeToParcel(Parcel out, int flags) {
+        // Note: order matters
+        out.writeString(title);
+        out.writeString(information);
+        out.writeParcelable(linkingIntent, 0);
+    }
+
+    /**
+     * Implement for Parcelable
+     * @return
+     */
+    public static final Parcelable.Creator<Detail> CREATOR
+            = new Parcelable.Creator<Detail>() {
+        public Detail createFromParcel(Parcel in) {
+            return new Detail(in);
+        }
+
+        public Detail[] newArray(int size) {
+            return new Detail[size];
+        }
+    };
+
+    /**
+     * Implement for Parcelable
+     * @return
+     */
+    private Detail(Parcel in) {
+        title = in.readString();
+        information = in.readString();
+        linkingIntent = in.readParcelable(Intent.class.getClassLoader());
     }
 }
