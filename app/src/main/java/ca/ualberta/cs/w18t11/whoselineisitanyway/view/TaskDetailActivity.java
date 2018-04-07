45962fb11d75b11e942e4af7d2a6f191ddf2a86e
Brad Ofrim <bofrim@gmail.com>
2018-04-07 13:39:51 -0600

    Adds buttons to task sceen based on situation

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/view/TaskDetailActivity.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/view/TaskDetailActivity.java
@@ -1,41 +1,308 @@
 package ca.ualberta.cs.w18t11.whoselineisitanyway.view;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import ca.ualberta.cs.w18t11.whoselineisitanyway.R;
+import ca.ualberta.cs.w18t11.whoselineisitanyway.controller.DataSourceManager;
+import ca.ualberta.cs.w18t11.whoselineisitanyway.model.task.Task;
+import ca.ualberta.cs.w18t11.whoselineisitanyway.model.user.User;
 
 /**
  * A custom DetailActivity for a task.
  */
 
 public class TaskDetailActivity extends DetailActivity {
+
+    /**
+     * Alter the appearance of the UI to better suit a task.
+     * @param viewGroup Parent for adding interface elements.
+     */
     public void customizeUserInterface(ViewGroup viewGroup)
     {
-        addBidButton(viewGroup);
+        try
+        {
+            Task task = getTaskFromIntent();
+            renderBasedOnTask(task, viewGroup);
+        }
+        catch(NullPointerException E)
+        {
+            // Don't do anything. Only render basic version.
+        }
     }
 
-    private void addBidButton(ViewGroup viewGroup)
+    /**
+     * Change what buttons and other UI elements are presented based on who's task it is.
+     * @param task to be rendered.
+     * @param viewGroup Parent for adding interface elements.
+     */
+    private void renderBasedOnTask(Task task, ViewGroup viewGroup)
+    {
+        DataSourceManager dataSourceManager = new DataSourceManager(this);
+        User currentUser = dataSourceManager.getCurrentUser();
+        if(currentUser.getUsername().equals(task.getRequesterUsername()))
+        {
+            renderMyTask(task, viewGroup);
+        }
+        else
+        {
+            renderOtherTask(task, viewGroup);
+        }
+    }
+
+    /**
+     * Render the task that belongs to the current user.
+     * @param task to be rendered.
+     * @param viewGroup Parent for adding interface elements.
+     */
+    private void renderMyTask(Task task, ViewGroup viewGroup)
+    {
+        switch (task.getStatus())
+        {
+            case REQUESTED:
+                renderMyRequestedTask(viewGroup);
+                break;
+            case BIDDED:
+                renderMyBiddedTask(viewGroup);
+                break;
+            case ASSIGNED:
+                renderMyAssignedTask(viewGroup);
+                break;
+            case DONE:
+                renderMyDoneTask(viewGroup);
+                break;
+        }
+    }
+
+    /**
+     * Render the task that does not belongs to the current user
+     * @param task to be rendered.
+     * @param viewGroup Parent for adding interface elements.
+     */
+    private void renderOtherTask(Task task, ViewGroup viewGroup)
+    {
+        switch (task.getStatus())
+        {
+            case REQUESTED:
+                renderOtherRequestedTask(viewGroup);
+                break;
+            case BIDDED:
+                renderOtherBiddedTask(viewGroup);
+                break;
+            case ASSIGNED:
+                renderOtherAssignedTask(viewGroup);
+                break;
+            case DONE:
+                renderOtherDoneTask(viewGroup);
+                break;
+        }
+    }
+
+    /**
+     * Render one of the current user's tasks that has a status of 'Requested'.
+     * Show the edit and delete buttons.
+     * @param viewGroup Parent for adding interface elements.
+     */
+    private void renderMyRequestedTask(ViewGroup viewGroup)
+    {
+
+        addEditTaskButton(viewGroup, 0);
+        addDeleteTaskButton(viewGroup, 1);
+    }
+
+    /**
+     * Render one of the current user's tasks that has a status of 'Bidded'.
+     * Show the edit, delete, and accept buttons.
+     * @param viewGroup Parent for adding interface elements.
+     */
+    private void renderMyBiddedTask(ViewGroup viewGroup)
+    {
+        addEditTaskButton(viewGroup, 0);
+        addDeleteTaskButton(viewGroup, 1);
+        addAcceptBidButton(viewGroup, 2);
+    }
+
+    /**
+     * Render one of the current user's tasks that has a status of 'Assigned'.
+     * Show the complete button.
+     * @param viewGroup Parent for adding interface elements.
+     */
+    private void renderMyAssignedTask(ViewGroup viewGroup)
+    {
+        addCompleteTaskButton(viewGroup, 0);
+        addUnassignTaskButton(viewGroup, 1);
+    }
+
+    /**
+     * Render one of the current user's tasks that has a status of 'Done'.
+     *
+     * @param viewGroup Parent for adding interface elements.
+     */
+    private void renderMyDoneTask(ViewGroup viewGroup)
+    {
+        // Show archive button?
+    }
+
+    /**
+     * Render a task from a user that is not the current user with a status of 'Requested'.
+     * Show the complete button.
+     * @param viewGroup Parent for adding interface elements.
+     */
+    private void renderOtherRequestedTask(ViewGroup viewGroup)
+    {
+        //  * task is requested: show add bid
+        addBidButton(viewGroup, 0);
+    }
+
+    /**
+     * Render a task from a user that is not the current user with a status of 'Bidded'.
+     * Show the complete button.
+     * @param viewGroup Parent for adding interface elements.
+     */
+    private void renderOtherBiddedTask(ViewGroup viewGroup)
+    {
+        //  * task is bidded && current user has not bid: show bid
+        //  * task is bidded && current user has bid: show change bid, remove bid
+        addBidButton(viewGroup, 0);
+    }
+
+    /**
+     * Render a task from a user that is not the current user with a status of 'Assigned'.
+     * Show the complete button.
+     * @param viewGroup Parent for adding interface elements.
+     */
+    private void renderOtherAssignedTask(ViewGroup viewGroup)
+    {
+        //  * task is assigned, but not to current user: show nothing
+        //  * task is assigned to the current user: show ??
+    }
+
+    /**
+     * Render a task from a user that is not the current user with a status of 'Done'.
+     * Show the complete button.
+     * @param viewGroup Parent for adding interface elements.
+     */
+    private void renderOtherDoneTask(ViewGroup viewGroup)
+    {
+        // Show no additional UI elements
+    }
+
+    private void addBidButton(ViewGroup viewGroup, int index)
     {
         // Make a view for the button
         LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.activity_detail_bid_button, viewGroup);
-
-        // Configure it
-        Button bidButton = (Button) view.findViewById(R.id.button_place_bid);
+        View view = inflater.inflate(R.layout.activity_detail_button, viewGroup);
+        Button bidButton = (Button) view.findViewById(R.id.detail_button);
+        bidButton.setText(R.string.button_place_bid);
         bidButton.setOnClickListener(new View.OnClickListener() {
-            // Go to detail screen without sending data
             @Override
             public void onClick(View view) {
-
+                // TODO: Add bid button functionality
             }
         });
 
-        // Add it into the existing view
-        ViewGroup insertPoint = (ViewGroup) findViewById(R.id.activity_detail_group);
-        insertPoint.addView(view, 1, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        ViewGroup insertPoint = findViewById(R.id.header_linear_layout);
+
+        insertPoint.addView(view, index, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    }
+
+    private void addAcceptBidButton(ViewGroup viewGroup, int index)
+    {
+        // Make a view for the button
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.activity_detail_button, viewGroup);
+        Button bidButton = (Button) view.findViewById(R.id.detail_button);
+        bidButton.setText(R.string.button_accept_bid);
+        bidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Add bid button functionality
+            }
+        });
+
+        ViewGroup insertPoint = findViewById(R.id.header_linear_layout);
+        insertPoint.addView(view, index, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    }
+
+    private void addEditTaskButton(ViewGroup viewGroup, int index)
+    {
+        // Make a view for the button
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.activity_detail_button, viewGroup);
+        Button bidButton = (Button) view.findViewById(R.id.detail_button);
+        bidButton.setText(R.string.button_edit_task);
+        bidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Add bid button functionality
+            }
+        });
+
+        ViewGroup insertPoint = findViewById(R.id.header_linear_layout);
+        insertPoint.addView(view, index, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    }
+
+    private void addDeleteTaskButton(ViewGroup viewGroup, int index)
+    {
+        // Make a view for the button
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.activity_detail_button, viewGroup);
+        Button bidButton = (Button) view.findViewById(R.id.detail_button);
+        bidButton.setText(R.string.button_delete_task);
+        bidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Add bid button functionality
+            }
+        });
+
+        ViewGroup insertPoint = findViewById(R.id.header_linear_layout);
+        insertPoint.addView(view, index, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    }
+
+    private void addCompleteTaskButton(ViewGroup viewGroup, int index)
+    {
+        // Make a view for the button
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.activity_detail_button, viewGroup);
+        Button bidButton = (Button) view.findViewById(R.id.detail_button);
+        bidButton.setText(R.string.button_complete_task);
+        bidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Add bid button functionality
+            }
+        });
+
+        ViewGroup insertPoint = findViewById(R.id.header_linear_layout);
+        insertPoint.addView(view, index, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    }
+
+    private void addUnassignTaskButton(ViewGroup viewGroup, int index)
+    {
+        // Make a view for the button
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.activity_detail_button, viewGroup);
+        Button bidButton = (Button) view.findViewById(R.id.detail_button);
+        bidButton.setText(R.string.button_unassign_task);
+        bidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Add bid button functionality
+            }
+        });
+
+        ViewGroup insertPoint = findViewById(R.id.header_linear_layout);
+        insertPoint.addView(view, index, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    }
+
+    private Task getTaskFromIntent() throws NullPointerException
+    {
+        Intent detailIntent = getIntent();
+        return (Task) detailIntent.getSerializableExtra(Task.TASK_KEY);
     }
 }
