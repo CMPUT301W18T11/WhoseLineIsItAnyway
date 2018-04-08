bc67fa751141dba4c73370ceee3f3ff388f1aeea
Samuel Dolha <dolha@ualberta.ca>
2018-04-07 19:09:35 -0600

    Create LatLng adapter

---
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/model/location/Location.java
@@ -0,0 +1,44 @@
+package ca.ualberta.cs.w18t11.whoselineisitanyway.model.location;
+
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.Serializable;
+
+public final class Location implements Serializable
+{
+    private static final long serialVersionUID = 2838946884503347862L;
+
+    private final double latitude;
+
+    private final double longitude;
+
+    /**
+     * Adapts a Google Maps LatLng to an equivalent, Serializable type.
+     *
+     * @param latLng The LatLng to adapt.
+     * @see LatLng
+     */
+    public Location(@NonNull final LatLng latLng)
+    {
+        this.latitude = latLng.latitude;
+        this.longitude = latLng.longitude;
+    }
+
+    /**
+     * @return The latitude.
+     */
+    public final double getLatitude()
+    {
+        return this.latitude;
+    }
+
+    /**
+     * @return The longitude.
+     */
+    public final double getLongitude()
+    {
+        return this.longitude;
+    }
+}
