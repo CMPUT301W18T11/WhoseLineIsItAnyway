0660f211ea4c8b89539bcc92a5193c0e55f6dae7
Brad Ofrim <bofrim@gmail.com>
2018-04-09 13:33:54 -0600

    Double add task

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/controller/ElasticsearchTaskController.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/controller/ElasticsearchTaskController.java
@@ -95,7 +95,34 @@ public class ElasticsearchTaskController
                                 + task.getTitle());
 
                         task.setElasticId(result.getId());
-                        return Boolean.TRUE;
+
+                        Index newIndex = new Index.Builder(task)
+                                .index(Constants.ELASTICSEARCH_INDEX)
+                                .type(typeStr)
+                                .id(task.getElasticId())
+                                .build();
+
+                        try
+                        {
+                            result = client.execute(newIndex);
+                            if(result.isSucceeded())
+                            {
+                                return Boolean.TRUE;
+                            }
+                            else
+                            {
+                                Log.i("Elasticsearch Error",
+                                        "index missing or could not connect:" +
+                                                Integer.toString(result.getResponseCode()));
+                                return Boolean.FALSE;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            // Probably disconnected
+                            Log.i("Elasticsearch Error", "Unexpected exception: " +
+                                    e.toString());
+                        }
                     }
                     else
                     {
